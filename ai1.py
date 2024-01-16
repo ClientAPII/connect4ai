e65a66f066caa47d142ec8338345877c34ef3d0b
ClientAPI <simso0508@gmail.com>
2024-01-16 15:38:09 +0100

    ai 1.1

--- ai1.py
+++ ai1.py
@@ -3,7 +3,6 @@ import math
 import random
 from helper import *
 
-
 # Constants
 EMPTY = 0
 PLAYER_1 = 1
@@ -12,12 +11,10 @@ WINDOW_LENGTH = 4
 
 def score_position(board, piece):
     score = 0
-    # Score center column
     center_array = [int(i[N_COLS//2]) for i in board]
     center_count = center_array.count(piece)
     score += center_count * 3
 
-    # Score horizontal
     for row in range(N_ROWS):
         row_array = [int(i) for i in board[row]]
         for c in range(N_COLS-3):
@@ -29,7 +26,6 @@ def score_position(board, piece):
             elif window.count(piece) == 2 and window.count(EMPTY) == 2:
                 score += 2
 
-    # Score vertical
     for col in range(N_COLS):
         col_array = [int(board[row][col]) for row in range(N_ROWS)]
         for r in range(N_ROWS-3):
@@ -41,7 +37,6 @@ def score_position(board, piece):
             elif window.count(piece) == 2 and window.count(EMPTY) == 2:
                 score += 2
 
-    # Score positive sloped diagonal
     for r in range(N_ROWS-3):
         for c in range(N_COLS-3):
             window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
@@ -52,7 +47,6 @@ def score_position(board, piece):
             elif window.count(piece) == 2 and window.count(EMPTY) == 2:
                 score += 2
 
-    # Score negative sloped diagonal
     for r in range(N_ROWS-3, N_ROWS):
         for c in range(N_COLS-3):
             window = [board[r-i][c+i] for i in range(WINDOW_LENGTH)]
@@ -70,26 +64,23 @@ def get_next_open_row(board, col):
         if board[r][col] == EMPTY:
             return r
     return -1
+
 def winning_move(board, piece):
-    # Check horizontal locations for win
     for c in range(N_COLS-3):
         for r in range(N_ROWS):
             if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                 return True
 
-    # Check vertical locations for win
     for c in range(N_COLS):
         for r in range(N_ROWS-3):
             if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
                 return True
 
-    # Check positively sloped diagonals
     for c in range(N_COLS-3):
         for r in range(N_ROWS-3):
             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
                 return True
 
-    # Check negatively sloped diagonals
     for c in range(N_COLS-3):
         for r in range(3, N_ROWS):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
@@ -103,13 +94,11 @@ def get_valid_locations(board):
         if not column_is_full(board, col):
             valid_locations.append(col)
 
-    # Prioritize moves that complete or block a connection of four
     for col in valid_locations:
         if winning_move(board, col):
             valid_locations.remove(col)
             valid_locations.insert(0, col)
 
-    # Prioritize moves in the center of the board
     valid_locations.sort(key=lambda x: abs(x - N_COLS // 2))
 
     return valid_locations
@@ -126,9 +115,9 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
                 return (None, 10000000000000)
             elif winning_move(board, PLAYER_1):
                 return (None, -10000000000000)
-            else: # Game is over, no more valid moves
+            else:
                 return (None, 0)
-        else: # Depth is zero
+        else:
             return (None, score_position(board, PLAYER_2))
     if maximizingPlayer:
         value = -math.inf
@@ -146,7 +135,7 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
                 break
         return column, value
 
-    else: # Minimizing player
+    else:
         value = math.inf
         column = random.choice(valid_locations)
         for col in valid_locations:
